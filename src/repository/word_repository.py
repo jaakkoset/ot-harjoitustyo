43ca6b2aa6dcf101118d232e79f5161f8be8b70f
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-03 19:28:08 +0200

    Clean code

--- src/repository/word_repository.py
+++ src/repository/word_repository.py
@@ -9,39 +9,6 @@ class WordRepository:
     def __init__(self, connection):
         self._connection = connection
 
-        self.words = [
-            {"id": "0", "latin": "puella", "translations": ("tyttö",)},
-            {
-                "id": "1",
-                "latin": "puer",
-                "translations": (
-                    "poika",
-                    "lapsi",
-                ),
-            },
-            {"id": "2", "latin": "bellum", "translations": ("sota",)},
-            {"id": "3", "latin": "rēx", "translations": ("kuningas",)},
-        ]
-
-        # self.words2 = [
-        #     ("puella", ("tyttö")),
-        #     ("colōnus", ("maanviljelijä")),
-        #     ("puer", ("poika", "lapsi")),
-        #     ("bellum", ("sota")),
-        #     ("rēx", ("kuningas")),
-        #     ("lītus", ("ranta")),
-        #     ("nāvis", ("laiva")),
-        #     ("mare", ("meri")),
-        #     ("bonus", ("hyvä")),
-        #     ("pauper", ("köyhä")),
-        #     ("omnis", ("jokainen")),
-        #     ("parō", ("valmistaa", "valmistautua")),
-        #     ("moneō", ("varoittaa")),
-        #     ("regō", ("hallita")),
-        #     ("audiō", ("kuulla")),
-        #     ("capiō", ("ottaa")),
-        # ]
-
     def get_word_test_words(self, word_test_id: str) -> tuple[dict, list[dict]]:
         """
         Return the exercise info, questions and correct answers.
@@ -101,11 +68,6 @@ class WordRepository:
             all_exercise_questions.append(question)
 
         return exercise, all_exercise_questions
-
-    def words_with_translations(self):
-        """Return the number of latin words that have a translation in the database.
-        Only words with translations are suitable for word tests."""
-        return len(self.words)
 
 
 word_repository = WordRepository(get_database_connection())
--- tasks.py
+++ tasks.py
@@ -16,14 +16,17 @@ def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
 
+
 @task
 def coverage_terminal(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage report -m", pty=True)
 
+
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
+
 
 @task
 def init_db(ctx):
