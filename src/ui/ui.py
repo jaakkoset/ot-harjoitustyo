9e2989cd1dfdef0db75a9ac23537f8c904d60dc5
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-03 21:05:13 +0200

    Use command design pattern for the ui

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,69 +1,105 @@
-from exercises.word_test import WordTest
+import sys
+from exercises.word_test import WordTest as WT
 from repository.stats_repository import stats_repository
 
 
 COMMANDS = {
     "q": "q lopeta ohjelma",
     "1": "1 sanakoe",
-    "2": "2 tunnista sanan taivutusmuoto",
-    "3": "3 tunnista lauserakenne",
     "4": "4 näytä tilastot",
 }
 
 
 class Ui:
-    """Program loop and its helper methods"""
-
     def __init__(self, io, stats_repo=stats_repository):
-        self._io = io
+        self.io = io
         self.stats = stats_repo
 
+        self.command_handler = CommandHandler(io)
+
     def program(self):
-        """Program loop"""
+        """The program loop"""
         while True:
             self._print_commands()
-            command = self._io.read("Anna komento: ")
-
-            if command == "q":
-                break
-
-            if command == "1":
-                self._word_test()
-
-            if command == "4":
-                self._stats()
+            command = self.io.read("Anna komento: ")
+            self.command_handler.get(command).run()
 
     def _print_commands(self):
         """Print all commands for the user"""
-        self._io.write("\nKomennot:")
+        self.io.write("\nKomennot:")
         for command in COMMANDS.values():
-            self._io.write("  " + command)
+            self.io.write("  " + command)
 
-    def _word_test(self):
-        """Ui for the word test."""
+
+class CommandHandler:
+    def __init__(self, io):
+        self.io = io
+
+        self.COMMANDS = {
+            "q": Quit(self.io),
+            "1": WordTest(self.io),
+            "4": Stats(self.io),
+        }
+
+    def get(self, command):
+        if command in self.COMMANDS:
+            return self.COMMANDS[command]
+
+        return InvalidCommand(self.io)
+
+
+class WordTest:
+    def __init__(self, io):
+        self.io = io
+
+    def run(self):
         word_test_id = 1
-        test = WordTest(word_test_id)
-        self._io.write("\nPoistu kirjoittamalla x")
+        test = WT(word_test_id)
+        self.io.write("\nPoistu kirjoittamalla x")
 
         while True:
             latin_word = test.latin_word()
-            self._io.write("\nSuomenna sana: " + latin_word)
-            answer = self._io.read()
+            self.io.write("\nSuomenna sana: " + latin_word)
+            answer = self.io.read()
             if answer == "x":
+                self.io.write("\nLopetit kokeen")
                 break
             answer_is_correct = test.check_answer(answer)
             if answer_is_correct:
-                self._io.write("\nVastasit oikein")
-                self._io.write("Oikeat vastaukset:")
-                self._io.write(test.printable_translations())
+                self.io.write("\nVastasit oikein")
+                self.io.write("Oikeat vastaukset:")
+                self.io.write(test.printable_translations())
                 if not test.change_to_next_word():
-                    self._io.write("\nKoe valmis")
+                    self.io.write("\nKoe valmis")
                     break
 
             else:
-                self._io.write("\nVastaus väärin")
+                self.io.write("\nVastaus väärin, yritä uudelleen.")
 
-    def _stats(self):
+
+class Stats:
+    def __init__(self, io):
+        self.io = io
+        self.stats = stats_repository
+
+    def run(self):
         """Show statistics about the user"""
         correct_answers = self.stats.get_total_correct_word_test_answers()
-        self._io.write(f"\nOlet vastannut oikein {correct_answers} sanaan")
+        self.io.write(f"\nOlet vastannut oikein {correct_answers} sanaan")
+
+
+class InvalidCommand:
+    def __init__(self, io):
+        self.io = io
+
+    def run(self):
+        self.io.write("\nVäärä komento")
+
+
+class Quit:
+    def __init__(self, io):
+        self.io = io
+
+    def run(self):
+        self.io.write("\nNäkemiin")
+        sys.exit(0)
