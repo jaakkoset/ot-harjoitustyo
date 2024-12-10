6e03d5a9379f45fc4095c821d266e7ed4901cbb8
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-10 23:41:22 +0200

    Add a test

--- src/tests/exercise_repository_test.py
+++ src/tests/exercise_repository_test.py
@@ -10,3 +10,11 @@ class TestExerciseRepository(unittest.TestCase):
         """get_exercise_questions returns a list"""
         words = self.repository.get_exercise_questions(1)
         self.assertIsInstance(words, list)
+
+    def test_get_exercise_info(self):
+        """Method get_exercise_info returns the correct info in a diciotnary"""
+        exercise = self.repository.get_exercise_info(1)
+        should_be = "Helppo sanakoe"
+        self.assertEqual(should_be, exercise["name"])
+        should_be = "Suomenna annetut sanat"
+        self.assertEqual(should_be, exercise["guide"])
