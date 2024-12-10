234f4fe9b3e379fa9c7f2c05f44084d6d819ac85
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-10 23:23:02 +0200

    Add tests for Exercise

--- src/tests/exercise_test.py
+++ src/tests/exercise_test.py
@@ -32,6 +32,19 @@ class TestExercise(unittest.TestCase):
         should_be = "  poika  lapsi"
         self.assertEqual(should_be, next_word)
 
+    def test_change_to_next_question_returns_false(self):
+        """Method change_to_next_question returns False after all questions have been
+        iterated"""
+        self.assertTrue(self.test.change_to_next_question())
+        self.assertFalse(self.test.change_to_next_question())
+
+    def test_check_answer(self):
+        """Method check_answer returns True when the answer is true and False otherwise
+        """
+        wrong_answer = self.test.check_answer("tyllerö")
+        self.assertFalse(wrong_answer)
+        correct_answer = self.test.check_answer("tyttö")
+        self.assertTrue(correct_answer)
 
 class StubExerciseRepository:
     def __init__(self):
