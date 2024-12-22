0c458063cfd62c3cd3bcae3be31550ce03c50d02
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-22 18:15:30 +0200

    All statistics are saved again

--- src/services/word_test.py
+++ src/services/word_test.py
@@ -13,6 +13,8 @@ class WordTestService:
         exercise.
         question_index: an integer count of the questions that have been answered.
         correct_answers: an integer count of the correct answers given by the user.
+        wordtest_completion_added_to_stats: True if user has answered all questions
+        correctly and this has been updated to statistcs, and False otherwise.
     """
 
     def __init__(
@@ -26,6 +28,7 @@ class WordTestService:
         self.exercise = None
         self.question_index = 0
         self.correct_answers = 0
+        self.__wordtest_completion_added_to_stats = False
 
     def new_exercise(self, exercise_id, exercise_repo=exercise_repository) -> None:
         """Create a new exercise.
@@ -43,10 +46,25 @@ class WordTestService:
         Return True when the answer is correct and False otherwise."""
         answer_is_correct = answer in self.exercise.answers(self.question_index)
         if answer_is_correct:
-            self.add_correct_word_test_answer_to_stats()
+            self.__is_word_test_completed()
+            self.__add_correct_word_test_answer_to_stats()
             return True
-        self.add_wrong_word_test_answer_to_stats()
+        self.__add_wrong_word_test_answer_to_stats()
         return False
+
+    def __is_word_test_completed(self):
+        """Check whether the user has answered all questions correctly. If so, add this
+        the statistics."""
+        if not self.__wordtest_completion_added_to_stats:
+            self.correct_answers += 1
+
+            all_questions_have_been_answered_correctly = (
+                self.correct_answers >= self.exercise.number_of_questions()
+            )
+
+            if all_questions_have_been_answered_correctly:
+                self.__wordtest_completion_added_to_stats = True
+                self.__add_completed_word_test_to_stats()
 
     def change_to_next_question(self) -> bool:
         """
@@ -74,11 +92,11 @@ class WordTestService:
         dictionary that has id, name, guide and type of the word test."""
         return self.exercise_repo.get_all_exercises("word test")
 
-    def add_correct_word_test_answer_to_stats(self):
+    def __add_correct_word_test_answer_to_stats(self):
         self.stats_repo.add_correct_word_test_answer()
 
-    def add_wrong_word_test_answer_to_stats(self):
+    def __add_wrong_word_test_answer_to_stats(self):
         self.stats_repo.add_wrong_word_test_answer()
 
-    def add_completed_word_test_to_stats(self):
+    def __add_completed_word_test_to_stats(self):
         self.stats_repo.add_completed_word_test()
