93a424767221de8a491fee2aab80b70b191c1a01
Jaakko Airikkala <jaakkel@gmail.com>
2024-12-22 15:07:40 +0200

    Encapsulate attributes of class Exercise

--- src/entities/exercise.py
+++ src/entities/exercise.py
@@ -22,39 +22,39 @@ class Exercise:
         self.exercise_id = exercise_id
         self.exercise_repo = exercise_repo
 
-        self.questions = self.exercise_repo.get_exercise_questions(exercise_id)
-        self.exercise_info = self.exercise_repo.get_exercise_info(exercise_id)
-        self.number_of_question = len(self.questions)
-        self.question_index = 0
+        self.__questions = self.exercise_repo.get_exercise_questions(exercise_id)
+        self.__exercise_info = self.exercise_repo.get_exercise_info(exercise_id)
+        self.__number_of_question = len(self.__questions)
+        self.__question_index = 0
 
     def change_to_next_question(self) -> bool:
         """
         Change the current question. Return True if question is changed and False if
         there are no questions left."""
-        self.question_index += 1
-        no_questions_left = self.question_index >= self.number_of_question
+        self.__question_index += 1
+        no_questions_left = self.__question_index >= self.__number_of_question
         if no_questions_left:
             return False
         return True
 
     def question(self) -> str:
         """Return the current question"""
-        return self.questions[self.question_index]["question"]
+        return self.__questions[self.__question_index]["question"]
 
     def answers(self) -> tuple:
         """Return the answers in a tuple"""
-        return self.questions[self.question_index]["answers"]
+        return self.__questions[self.__question_index]["answers"]
 
     def printable_answers(self) -> str:
         """Return the answers in one string"""
         text = ""
-        for t in self.questions[self.question_index]["answers"]:
+        for t in self.__questions[self.__question_index]["answers"]:
             text += "  " + t
         return text
 
     def check_answer(self, answer) -> bool:
         """Check the answer given by the user. Return True when the answer is correct
         and False otherwise."""
-        if answer in self.questions[self.question_index]["answers"]:
+        if answer in self.__questions[self.__question_index]["answers"]:
             return True
         return False
